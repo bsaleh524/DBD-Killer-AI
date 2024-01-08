ebaaa6473fdc34ddb153e682be35070ba68429af
Basem Saleh <mreagles524@gmail.com>
2024-01-07 20:37:05 -0500

    Moved over dataset download to proper function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,42 @@
 # -*- coding: utf-8 -*-
 import click
 import logging
+from roboflow import Roboflow
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+def main(rf_data_version: int=3, data_format: str = "yolov8"):
+    """ Grabs data from RoboFlow, if not already downloaded to the 
+        current directory.
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+    # Pull data from roboflow
+    rf = Roboflow(api_key=os.environ.get("RF_API_KEY"))
+    project = rf.workspace(os.environ.get("RF_WORKSPACE")).project(os.environ.get("RF_PROJECT"))
+
+    dataset = project.version(rf_data_version).download(data_format, location=str(project_dir))
+    print(dataset.location)
+
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
+# def main_given(input_filepath, output_filepath):
+#     """ Grabs data from RoboFlow, if not already downloaded to the 
+#         current directory.
+    
+    
+    
+#         Runs data processing scripts to turn raw data from (../raw) into
+#         cleaned data ready to be analyzed (saved in ../processed).
+#     """
+#     logger = logging.getLogger(__name__)
+#     logger.info('making final data set from raw data')
 
 
 if __name__ == '__main__':
@@ -23,8 +46,10 @@ if __name__ == '__main__':
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
 
+    # Add roboflow data to the 'external' folder
+    project_dir = project_dir.joinpath("data/external")
+
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
-
     main()
---
+++ todo.md
@@ -0,0 +1,8 @@
+## TODO
+
+1. Make the env file for storing keys
+2. Use `make_dataset.py` with the first part of the notebok
+3. 
+
+
+- notebook is for exploring data, not training it.

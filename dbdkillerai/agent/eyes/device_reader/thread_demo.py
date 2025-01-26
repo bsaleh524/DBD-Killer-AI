6bcc4b209489e8e28b2d97dd9a2343e7218aeba0
Basem Saleh <mreagles524@gmail.com>
2025-01-25 19:02:51 -0500

    added the correct test scripts to thread the camera

--- dbdkillerai/agent/eyes/device_reader/thread_demo.py
+++ dbdkillerai/agent/eyes/device_reader/thread_demo.py
@@ -1,73 +1,8 @@
-import argparse
-import os
+from dbdkillerai.agent.eyes.device_reader.vid_show_and_tell import (
+    VideoGet, VideoShow)
+import platform
 import cv2
-from CountsPerSec import CountsPerSec
-from VideoGet import VideoGet
-from VideoShow import VideoShow
 
-def putIterationsPerSec(frame, iterations_per_sec):
-    """
-    Add iterations per second text to lower-left corner of a frame.
-    """
-
-    cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
-        (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
-    return frame
-
-def noThreading(source=0):
-    """Grab and show video frames without multithreading."""
-
-    cap = cv2.VideoCapture(source)
-    cps = CountsPerSec().start()
-
-    while True:
-        grabbed, frame = cap.read()
-        if not grabbed or cv2.waitKey(1) == ord("q"):
-            break
-
-        frame = putIterationsPerSec(frame, cps.countsPerSec())
-        cv2.imshow("Video", frame)
-        cps.increment()
-
-def threadVideoGet(source=0):
-    """
-    Dedicated thread for grabbing video frames with VideoGet object.
-    Main thread shows video frames.
-    """
-
-    video_getter = VideoGet(source).start()
-    cps = CountsPerSec().start()
-
-    while True:
-        if (cv2.waitKey(1) == ord("q")) or video_getter.stopped:
-            video_getter.stop()
-            break
-
-        frame = video_getter.frame
-        frame = putIterationsPerSec(frame, cps.countsPerSec())
-        cv2.imshow("Video", frame)
-        cps.increment()
-
-def threadVideoShow(source=0):
-    """
-    Dedicated thread for showing video frames with VideoShow object.
-    Main thread grabs video frames.
-    """
-
-    cap = cv2.VideoCapture(source)
-    (grabbed, frame) = cap.read()
-    video_shower = VideoShow(frame).start()
-    cps = CountsPerSec().start()
-
-    while True:
-        (grabbed, frame) = cap.read()
-        if not grabbed or video_shower.stopped:
-            video_shower.stop()
-            break
-
-        frame = putIterationsPerSec(frame, cps.countsPerSec())
-        video_shower.frame = frame
-        cps.increment()
 
 def threadBoth(source=0):
     """
@@ -79,7 +14,6 @@ def threadBoth(source=0):
 
     video_getter = VideoGet(source).start()
     video_shower = VideoShow(video_getter.frame).start()
-    cps = CountsPerSec().start()
 
     while True:
         if video_getter.stopped or video_shower.stopped:
@@ -88,40 +22,31 @@ def threadBoth(source=0):
             break
 
         frame = video_getter.frame
-        frame = putIterationsPerSec(frame, cps.countsPerSec())
         video_shower.frame = frame
-        cps.increment()
 
-def main():
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--source", "-s", default=0,
-        help="Path to video file or integer representing webcam index"
-            + " (default 0).")
-    ap.add_argument("--thread", "-t", default="none",
-        help="Threading mode: get (video read in its own thread),"
-            + " show (video show in its own thread), both"
-            + " (video read and video show in their own threads),"
-            + " none (default--no multithreading)")
-    args = vars(ap.parse_args())
+def threadVideoGet(source=0):
+    """
+    Dedicated thread for grabbing video frames with VideoGet object.
+    Main thread shows video frames.
+    """
 
-    # If source is a string consisting only of integers, check that it doesn't
-    # refer to a file. If it doesn't, assume it's an integer camera ID and
-    # convert to int.
-    if (
-        isinstance(args["source"], str)
-        and args["source"].isdigit()
-        and not os.path.isfile(args["source"])
-    ):
-        args["source"] = int(args["source"])
+    video_getter = VideoGet(source).start()
 
-    if args["thread"] == "both":
-        threadBoth(args["source"])
-    elif args["thread"] == "get":
-        threadVideoGet(args["source"])
-    elif args["thread"] == "show":
-        threadVideoShow(args["source"])
-    else:
-        noThreading(args["source"])
+    while True:
+        if (cv2.waitKey(1) == ord("q")) or video_getter.stopped:
+            video_getter.stop()
+            break
+
+        frame = video_getter.frame
+        cv2.imshow("Video", frame)
+
+
 
 if __name__ == "__main__":
-    main()
+    device = 1
+    if platform.system() != "Darwin":
+        threadBoth(source=device)
+    else:
+        print("We on the bigMac now huh?")
+        threadVideoGet(device)
+

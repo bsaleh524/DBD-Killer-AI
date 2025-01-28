f97c2332e1dff97f0fd429ef8236f5df5867df38
Basem Saleh <mreagles524@gmail.com>
2025-01-27 20:26:23 -0500

    added stuff together. must do more

--- dbdkillerai/agent/eyes/device_reader/thread_demo.py
+++ dbdkillerai/agent/eyes/device_reader/thread_demo.py
@@ -4,6 +4,40 @@ import platform
 import cv2
 
 
+def customthreadBoth(source=0):
+    """
+    Dedicated thread for grabbing video frames with VideoGet object.
+    Dedicated thread for showing video frames with VideoShow object.
+    """
+    pass
+
+def threadVideoShow(source=0, yolo_results=None, class_labels=None):
+    """
+    Dedicated thread for showing video frames with VideoShow object.
+    Main thread grabs video frames.
+    """
+
+    cap = cv2.VideoCapture(source)
+    (grabbed, frame) = cap.read()
+    video_shower = VideoShow(frame).start()
+
+    while True:
+        (grabbed, frame) = cap.read()
+        if not grabbed or video_shower.stopped:
+            video_shower.stop()
+            break
+        
+        if yolo_results:
+            for result in yolo_results[0].boxes:
+                    x1, y1, x2, y2 = map(int, result.xyxy[0])
+                    cls = int(result.cls)
+                    conf = float(result.conf)
+                    label = f"{class_labels[cls]} {conf:.2f}"
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 2)
+                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        video_shower.frame = frame
+
 def threadBoth(source=0):
     """
     Dedicated thread for grabbing video frames with VideoGet object.

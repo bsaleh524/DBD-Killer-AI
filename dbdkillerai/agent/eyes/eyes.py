32cc1599b1d3e6193f399f8d9b95b53533fe34e0
Basem Saleh <mreagles524@gmail.com>
2025-01-27 21:45:49 -0500

    added final changes, for sure

--- dbdkillerai/agent/eyes/eyes.py
+++ dbdkillerai/agent/eyes/eyes.py
@@ -1,3 +1,6 @@
+from threading import Thread
+from queue import Queue
+from dbdkillerai.agent.eyes.ocr.text_reader import setup_camera
 import cv2
 
 def read_screen_capture(capture_device, vision_queue):
@@ -8,6 +11,55 @@ def read_screen_capture(capture_device, vision_queue):
         if not ret:
             raise("Failed to grab frame. Check devices!")
         # return frame
+
+class VideoGet:
+    """
+    Class that continuously gets frames from a VideoCapture object
+    with a dedicated thread.
+    """
+
+    def __init__(self,
+                 device=0,
+                 test_image=False,
+                 vid_preset=0):
+        
+        # Get presets for video quality. Small or Large
+        if vid_preset == 0:
+            height=640,
+            width=480
+        elif vid_preset == 1:
+            height=720,
+            width=1280
+        
+        # Setup the camera
+        self.stream = setup_camera(test_image=test_image,
+                                    device=device,
+                                    height=height,
+                                    width=width,
+            )
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
+        self.queue = Queue()
+
+    def start(self):    
+        Thread(target=self.get, args=()).start()
+        return self
+
+    def get(self):
+        while not self.stopped:
+            if not self.grabbed:
+                self.stop()
+            else:
+                (self.grabbed, self.frame) = self.stream.read()
+                self.queue.put(self.frame)
+
+    def stop(self):
+        self.stopped = True
+        self.stream.release()
+    
+    def get_fps(self):
+        return self.stream.get(cv2.CAP_PROP_FPS)
+
 
 if __name__ == "__main__":
     pass
